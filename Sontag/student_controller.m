879a815d777e945d68c526faab6a871af11dbd14
Noah Fox <fox.noah@gmail.com>
2015-05-08 19:30:06 -0400

    ..

--- Sontag/student_controller.m
+++ Sontag/student_controller.m
@@ -9,24 +9,38 @@
 function u = student_controller(t, x, consts, ctrl)
 global done
 
-if done == 0
+% LfV = eval(subs(ctrl.LfV,sym_vec,x));
+% LgV = eval(subs(ctrl.LgV,sym_vec,x));
+
+
+if done == 0 && x(2) > 200
     x(2) = x(2) - 200;
 end
 
+
 LfV = ctrl.LfVfun(x(1),x(2),x(3),x(4),x(5),x(6),x(7),x(8),x(9));
 LgV = ctrl.LgVfun(x(1),x(2),x(3),x(4),x(5),x(6),x(7),x(8),x(9));
+% a = LfV;
+% b = LgV*LgV';
+% if LgV ~= 0
+%     u = -((a + sqrt(a^2 + b^4))/b)*LgV';
+% else
+%     u = [0;0];
+% end
 
 if all(LgV ~= 0)
+   %{ 
+    u(1,1) = -((LfV + sqrt(LfV^2 + LgV(1)^4))/LgV(1));
+    u(2,1) = -((LfV + sqrt(LfV^2 + LgV(2)^4))/LgV(2));
+%}
     u = -LgV'*(LfV + sqrt(LfV^2 + (LgV*LgV')^2))/(LgV*LgV');
 else
     u = [0;0];
 end
 % && abs(x(5)) < 5
-if abs(x(3)) < 0.2 && abs(x(7)) < .1 && abs(x(5)) < 50 || done == 1
+% if abs(x(3)) < 0.05 && abs(x(7)) < .1 && abs(x(2)) < 100 || done == 1
+if abs(x(3)) < 0.05 && abs(x(7)) < .1 && abs(x(2)) < 40 || done == 1
     u = -ctrl.K*x;
-    if done == 0
-        disp('Switched')
-    end
     done = 1;
 end
 end
